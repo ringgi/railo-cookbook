0bd704422f6c64ff1793ec703422d54d50bc5ede
Simon Hooker <simon@mso.net>
2014-11-12 11:43:42 +0000

    Add an opsworks dependency in

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,7 @@ description       'Install and setup Railo'
 long_description  'The cookbook installs Railo on an existing Java Application Server'
 version           '0.1.2'
 
+depends 'opsworks_java'
+
 recipe 'railo::default', 'Runs the railo::tomcat_server recipe'
 recipe 'railo::tomcat_server', 'Installs Railo as a webserver on Tomcat'
